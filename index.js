a239504ec78ff948b790bec13825e753df15f4ee
22shashank <166897158+22shashank@users.noreply.github.com>
2024-09-10 09:41:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function addToCart(item) {
+    alert(item + ' has been added to your cart!');
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
